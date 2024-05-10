af2d0d0dec9a039113158f01c442e37874be5e78
Sergey Semenov <semenov.web.sup@gmail.com>
2024-05-10 13:54:03 +0200

    added svgr/webpack

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { Button } from '@/components/Button/Button';
 import {Htag} from '../components'
+import Image from 'next/image';
+import Logo from '../public/vercel.svg';
 
 export default function Home() {
 
@@ -8,6 +10,13 @@ export default function Home() {
       <Htag tag = 'h1'>Заголовок</Htag>
       <Button appearance='primary' className='new'>кнопка</Button>
       <Button appearance='ghost'>кнопка</Button>
+      <Image
+        src='/vercel.svg'
+        width={100}
+        height={24}
+        alt=''
+      />
+      <Logo />
     </>
   );
 }
