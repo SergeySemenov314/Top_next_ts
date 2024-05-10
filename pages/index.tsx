3d53231c718007c5d2c42c347d6c26a7e5f37a04
Sergey Semenov <semenov.web.sup@gmail.com>
2024-05-10 11:02:38 +0200

    created Button component, added classnames lib

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/Button/Button';
 import {Htag} from '../components'
 
 export default function Home() {
@@ -5,6 +6,8 @@ export default function Home() {
   return (
     <>
       <Htag tag = 'h1'>Заголовок</Htag>
+      <Button appearance='primary'>кнопка</Button>
+      <Button appearance='ghost'>кнопка</Button>
     </>
   );
 }
