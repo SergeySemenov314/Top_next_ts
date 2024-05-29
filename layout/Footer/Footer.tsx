734498352d8503d9d051170524656b191b1a4445
Sergey Semenov <semenov.web.sup@gmail.com>
2024-05-29 11:02:42 +0200

    created html, css for footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,11 +1,14 @@
 import styles from "./Footer.module.css";
 import { FooterProps } from "./Footer.props";
 import cn from "classnames";
+import {format} from 'date-fns'
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props }: FooterProps): JSX.Element => {
     return (
-        <div {...props}>
-            Footer
+        <div className={cn(className, styles.footer)} {...props}>
+            <div className={styles.copyright}>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+            <a href="/" className={styles.personal}>Пользовательское соглашение</a>
+            <a href="/" className={styles.confidential}>Политика конфиденциальности</a>
         </div>
     );
 };
